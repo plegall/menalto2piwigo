f20cac37d062a9df523300d5f9d0518af51de689
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-06-29 18:30:43 +0000

    [menalto2piwigo] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/menalto2piwigo@28855 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Menalto2Piwigo plugin imports data from a Menalto Gallery2 installation into Piwigo.'] = 'Menalto2Piwigo spraudnis importē datus Piwigo no Menalto Gallery2 instalācijas.';
+$lang['Submit the form at the end of this page'] = 'Apstiprināt formu šīs lapas beigās';
+$lang['column prefix'] = 'kolonas prefikss';
+$lang['database host'] = 'datubāzes hosts';
+$lang['database user'] = 'datubāzes lietotājs';
+$lang['table prefix'] = 'Tabulas prefikss';
+$lang['User comments on photos'] = 'Lietotāju komentāri par foto';
+$lang['Title and description for photos'] = 'Fotogrāfiju nosaukumi un apraksts';
+$lang['Name and description for albums'] = 'Albūmu nosaukumi un apraksts';
+$lang['database password'] = 'datubāzes parole';
+$lang['database name'] = 'datubāzes nosaukums';
+$lang['Start import'] = 'Sākt importu';
+$lang['Copy the content of g2data/albums into piwigo/galleries'] = 'Kopēt g2data/albums saturu piwigo/galleries';
+$lang['Install and activate plugins:'] = 'Instalēt un aktivēt spraudņus';
+$lang['Imported properties:'] = 'Importētās īpašības';
+$lang['Import from Menalto'] = 'Imports no Menalto';
+$lang['Import'] = 'imports';
+$lang['How to use it?'] = 'Kā to lietot?';
