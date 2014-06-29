47a9e30b9c9962a827e7866a50f2ef79d0bba1ba
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-06-29 06:29:55 +0000

    [menalto2piwigo] Update sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/menalto2piwigo@28828 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Menalto2Piwigo plugin imports data from a Menalto Gallery2 installation into Piwigo.'] = 'Menalto2Piwigo doplnky načítajú dáta z Menalto Gallery2 do Piwigo.';
+$lang['table prefix'] = 'prefix tabuľky';
+$lang['database user'] = 'používateľ databázy';
+$lang['database password'] = 'heslo databázy';
+$lang['database name'] = 'názov databázy';
+$lang['database host'] = 'host databázy';
+$lang['column prefix'] = 'prefix stĺpca';
+$lang['User comments on photos'] = 'Používateľove komentáre na fotky';
+$lang['Title and description for photos'] = 'Názov a popis fotiek';
+$lang['Submit the form at the end of this page'] = 'Odoslať formulár na konci tejto stránky';
+$lang['Start import'] = 'Spustiť načítanie';
+$lang['Name and description for albums'] = 'Názov a popis pre albumy';
+$lang['Install and activate plugins:'] = 'Inštalovať a aktivovať doplnky:';
+$lang['Imported properties:'] = 'Načítané vlastnosti:';
+$lang['Import from Menalto'] = 'Načítanie z Menalto';
+$lang['Import'] = 'Načítanie';
+$lang['How to use it?'] = 'Ako to použiť?';
+$lang['Copy the content of g2data/albums into piwigo/galleries'] = 'Kopírovať obsah g2data/albums do piwigo/galleries';
