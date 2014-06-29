b0540e97fafcc8d7341185873398d1a29750ffb7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-06-29 18:30:50 +0000

    [menalto2piwigo] Update pt_BR, thanks to : flaviove
    git-svn-id: http://piwigo.org/svn/extensions/menalto2piwigo@28860 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['table prefix'] = 'prefixo da tabela';
+$lang['database user'] = 'usuário do banco de dados';
+$lang['database password'] = 'senha do banco de dados';
+$lang['database name'] = 'nome do banco de dados';
+$lang['database host'] = 'banco de dados host';
+$lang['column prefix'] = 'coluna prefixo';
+$lang['User comments on photos'] = 'Comentários de usuários nas fotos';
+$lang['Title and description for photos'] = 'Título e descrição para fotos';
+$lang['Submit the form at the end of this page'] = 'Submeter o formulário para o final desta página';
+$lang['Start import'] = 'Iniciar importação';
+$lang['Name and description for albums'] = 'Nome e descrição para albuns';
+$lang['Menalto2Piwigo plugin imports data from a Menalto Gallery2 installation into Piwigo.'] = 'O plugin Menalto2Piwigo importa dados de instalação do Menalto Gallery2 para o Piwigo.';
+$lang['Install and activate plugins:'] = 'Instalar e ativar plugins:';
+$lang['Imported properties:'] = 'Propriedades importadas:';
+$lang['Import from Menalto'] = 'Importar de Menalto';
+$lang['Import'] = 'Importar';
+$lang['How to use it?'] = 'Como usar isto?';
+$lang['Copy the content of g2data/albums into piwigo/galleries'] = 'Copiar o conteúdo de g2data/albums para piwigo/gallaries';
