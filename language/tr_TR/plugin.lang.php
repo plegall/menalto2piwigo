59edfc825e905a5fbde0cbe90a485873d6c85d77
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-07-01 09:53:46 +0000

    [menalto2piwigo] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/menalto2piwigo@28884 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['column prefix'] = 'kolon ön eki';
+$lang['User comments on photos'] = 'Fotoğrafların kullanıcı yorumları';
+$lang['database name'] = 'veri tabanı adı';
+$lang['database password'] = 'veri tabanı şifresi';
+$lang['database user'] = 'veri tabanı kullanıcı adı';
+$lang['table prefix'] = 'tablo ön eki';
+$lang['How to use it?'] = 'Nasıl kullanırım ?';
+$lang['Import'] = 'İçe aktar';
+$lang['Import from Menalto'] = 'Menalto\'dan içe aktar';
+$lang['Imported properties:'] = 'İçe aktarılan öznitelikler:';
+$lang['Install and activate plugins:'] = 'Eklentileri yükle ve etkinleştir:';
+$lang['Start import'] = 'İçe aktarmayı başlat';
