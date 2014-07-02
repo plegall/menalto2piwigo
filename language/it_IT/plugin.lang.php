3fcd0877b264b0b7fc50d829875d2aa1b0ece73e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-07-02 14:18:50 +0000

    [menalto2piwigo] Update it_IT, thanks to : Ericnet
    git-svn-id: http://piwigo.org/svn/extensions/menalto2piwigo@28921 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Submit the form at the end of this page'] = 'Invia il modulo alla fine di questa pagina';
+$lang['database host'] = 'host database';
+$lang['database name'] = 'nome database';
+$lang['database password'] = 'password database';
+$lang['database user'] = 'nome utente database';
+$lang['table prefix'] = 'prefisso tabelle';
+$lang['Title and description for photos'] = 'Titolo e descrizione per le foto';
+$lang['User comments on photos'] = 'Commenti utenti sulle foto';
+$lang['column prefix'] = 'prefisso colonne';
+$lang['Start import'] = 'Avvia importazione';
+$lang['Menalto2Piwigo plugin imports data from a Menalto Gallery2 installation into Piwigo.'] = 'Plugin Menalto2Piwigo importa dati da una installazione di Menalto Gallery2 in Piwigo.';
+$lang['Name and description for albums'] = 'Nome e descrizione per gli album';
+$lang['Copy the content of g2data/albums into piwigo/galleries'] = 'Copia il contenuto di g2data/albums in piwigo/galleries';
+$lang['Import'] = 'Importa';
+$lang['Import from Menalto'] = 'Importa da Menalto';
+$lang['Imported properties:'] = 'Proprietà importati:';
+$lang['Install and activate plugins:'] = 'Installa e attiva plugin:';
+$lang['How to use it?'] = 'Come si usa?';
