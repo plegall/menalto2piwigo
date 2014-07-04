361ddb6ba1590ef37c59e9d5cba6c8d8f9ef1da2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-07-04 16:50:49 +0000

    [menalto2piwigo] Update zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/menalto2piwigo@28941 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Copy the content of g2data/albums into piwigo/galleries'] = '将 g2data/albums 中的内容复制到 piwigo/galleries';
+$lang['How to use it?'] = '如何使用？';
+$lang['Import'] = '导入';
+$lang['Import from Menalto'] = '从 Menalto 导入';
+$lang['Imported properties:'] = '已导入内容：';
+$lang['Install and activate plugins:'] = '安装并启用插件：';
+$lang['Menalto2Piwigo plugin imports data from a Menalto Gallery2 installation into Piwigo.'] = 'Menalto2Piwigo 插件用于将 Menalto Gallery2 中的数据导入 Piwigo。';
+$lang['Name and description for albums'] = '相册名称与描述';
+$lang['Start import'] = '开始导入';
+$lang['Submit the form at the end of this page'] = '提交本页底部的表格';
+$lang['Title and description for photos'] = '图片标题与描述';
+$lang['User comments on photos'] = '图片评论';
+$lang['column prefix'] = '列前缀';
+$lang['database host'] = '数据库服务器（database host）';
+$lang['database name'] = '数据库名称';
+$lang['database password'] = '数据库密码';
+$lang['database user'] = '数据库用户名';
+$lang['table prefix'] = '数据表前缀';
