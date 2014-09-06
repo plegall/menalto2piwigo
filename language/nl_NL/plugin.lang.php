087bff77ebd5e48ea6daaa7fee6db2aa4f2ef175
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-09-06 20:45:48 +0000

    [menalto2piwigo] Update nl_NL, thanks to : Kees Hessels
    git-svn-id: http://piwigo.org/svn/extensions/menalto2piwigo@29414 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Copy the content of g2data/albums (Gallery2) or var/albums (Gallery3) into piwigo/galleries'] = 'Kopieer de inhoud van de g2data/albums (Gallery2) of var/albums (Gallery3) naar piwigo/galerieën';
+$lang['How to use it?'] = 'Hoe te gebruiken?';
+$lang['Import'] = 'Importeren';
+$lang['Import from Menalto'] = 'Importeer van Menalto';
+$lang['Imported properties:'] = 'Geïmporteerde eigenschappen:';
+$lang['Install and activate plugins:'] = 'Installeer en activeer plugins:';
+$lang['Menalto2Piwigo plugin imports data from a Menalto Gallery2/Gallery3 installation into Piwigo.'] = 'Menalto2Piwigo plugins importeren data van een Menalto Gallery2/Gallery3 installatie naar Piwigo.';
+$lang['Name and description for albums'] = 'Naam en beschrijvingen van albums';
+$lang['No Menalto tables found!'] = 'Geen Menalto-tabellen gevonden!';
+$lang['Start import'] = 'Start het importeren';
+$lang['Submit the form at the end of this page'] = 'Verstuur het formulier op het einde van deze pagina';
+$lang['Title and description for photos'] = 'Titel en beschrijving van foto\'s';
+$lang['User comments on photos'] = 'Reacties van gebruikers op foto\'s';
+$lang['column prefix'] = 'kolom-voorvoegsel';
+$lang['database host'] = 'database-host';
+$lang['database name'] = 'databasenaam';
+$lang['database password'] = 'database-wachtwoord';
+$lang['database user'] = 'databasegebruiker';
+$lang['table prefix'] = 'tabel-voorvoegsel';
+$lang['useless on Gallery3'] = 'nutteloos voor Gallery3';
