d79e6b44f83264a568d6a68c12e59213a7bcbce6
Piwigo-TranslationTeam <Piwigo-TranslationTeam@users.noreply.github.com>
2014-11-25 19:08:05 +0100

    [menalto2piwigo] Update uk_UA, thanks to m_alien

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['useless on Gallery3'] = 'непотрібно для Gallery3';
+$lang['database host'] = 'хост бази даних';
+$lang['table prefix'] = 'префікс таблиці';
+$lang['column prefix'] = 'префікс колонки';
+$lang['Submit the form at the end of this page'] = 'Заповніть форму в кінці цієї сторінки';
+$lang['User comments on photos'] = 'Коментарі до світлин';
+$lang['Title and description for photos'] = 'Назва та опис світлин';
+$lang['Imported properties:'] = 'Імпортовані властивості:';
+$lang['Menalto2Piwigo plugin imports data from a Menalto Gallery2/Gallery3 installation into Piwigo.'] = 'Плагін Menalto2Piwigo імпортує дані з встановлених Menalto Gallery2/Gallery3 у Piwigo.';
+$lang['Install and activate plugins:'] = 'Встановіть та активуйте плагіни:';
+$lang['Copy the content of g2data/albums (Gallery2) or var/albums (Gallery3) into piwigo/galleries'] = 'Скопіюйте вміст g2data/albums (Gallery2) чи var/albums (Gallery3) у piwigo/galleries';
+$lang['No Menalto tables found!'] = 'Не знайдено таблиць Menalto!';
+$lang['Name and description for albums'] = 'Назва та опис для альбомів';
+$lang['database user'] = 'користувач бази даних';
+$lang['database password'] = 'пароль бази даних';
+$lang['database name'] = 'ім\'я бази даних';
+$lang['Start import'] = 'Почати імпорт';
+$lang['Import from Menalto'] = 'Імпортувати з Menalto';
+$lang['Import'] = 'Імпортувати';
+$lang['How to use it?'] = 'Як цим користуватись?';
