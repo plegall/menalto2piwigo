5a0a1f9ba93c9ae5bf00674864baddefb7db109c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-02-21 13:05:01 +0100

    Update sv_SE, thanks to perforator

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['useless on Gallery3'] = 'oanvändbart i Gallery3';
+$lang['table prefix'] = 'tabellprefix';
+$lang['database user'] = 'databasanvändare';
+$lang['database password'] = 'databaslösenord';
+$lang['database name'] = 'databasnamn';
+$lang['database host'] = 'databasvärd';
+$lang['column prefix'] = 'kolumnprefix';
+$lang['User comments on photos'] = 'Användarkommentarer för fotografier';
+$lang['Title and description for photos'] = 'Titel och beskrivning för fotografier';
+$lang['Submit the form at the end of this page'] = 'Skicka formuläret längst ner på sidan';
+$lang['Start import'] = 'Starta import';
+$lang['No Menalto tables found!'] = 'Inga Menalto tabeller upptäckta!';
+$lang['Name and description for albums'] = 'Namn och beskrivning för album';
+$lang['Menalto2Piwigo plugin imports data from a Menalto Gallery2/Gallery3 installation into Piwigo.'] = 'Menalto2Piwigo plugin importerar data från en Menalto Gallery2/Galler3 installation till Piwigo.';
+$lang['Install and activate plugins:'] = 'Installera och aktivera plugins:';
+$lang['Imported properties:'] = 'Importerade egenskaper:';
+$lang['Import from Menalto'] = 'Import från Menalto';
+$lang['Import'] = 'Import';
+$lang['How to use it?'] = 'Hur används det?';
+$lang['Copy the content of g2data/albums (Gallery2) or var/albums (Gallery3) into piwigo/galleries'] = 'Kopiera innehållet från "./g2data/albums" /Gallery2) eller "./var/albums" (Gallery3) till piwigo/galleries';
