1135988e5249cca7d2d41632408a7e2b9313f3cf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-12-28 19:10:05 +0100

    Add bg_BG, thanks to turnokopa

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['database user'] = 'потребител на базата данни';
+$lang['database password'] = 'парола на базата данни';
+$lang['database host'] = 'хостинг на базата данни';
+$lang['database name'] = 'име на базата данни';
+$lang['column prefix'] = 'префикс на колона';
+$lang['User comments on photos'] = 'Потребителски коментари към снимките';
+$lang['Title and description for photos'] = 'Название и описание за снимки';
+$lang['Submit the form at the end of this page'] = 'Потвърдете формата в края на тази страница';
+$lang['Start import'] = 'Начало на импорт';
+$lang['No Menalto tables found!'] = 'Не е намерена Menalto таблица!';
+$lang['Name and description for albums'] = 'Име и описание на албумите';
+$lang['Menalto2Piwigo plugin imports data from a Menalto Gallery2/Gallery3 installation into Piwigo.'] = 'Menalto2Piwigo импортира данните от инсталацията Menalto Gallery2/Gallery3 в Piwigo.';
+$lang['Copy the content of g2data/albums (Gallery2) or var/albums (Gallery3) into piwigo/galleries'] = 'Копиране на съдържанието от g2data/albums (Gallery2) или от var/albums (Gallery3) в piwigo/galleries';
+$lang['Install and activate plugins:'] = 'Инсталиране и активация на плъгините:';
+$lang['Imported properties:'] = 'Настройки на импортиране';
+$lang['Import from Menalto'] = 'Импорт от Menalto';
+$lang['Import'] = 'Импорт';
+$lang['How to use it?'] = 'Как да я използвате';
+$lang['useless on Gallery3'] = 'полезно в Gallery3';
+$lang['table prefix'] = 'префикс на таблица';
