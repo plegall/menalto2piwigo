7d230821dd953ee8f666a5ce85385277bf5a2de2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-01-27 21:11:19 +0100

    Add pl_PL, thanks to kuba

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Copy the content of g2data/albums (Gallery2) or var/albums (Gallery3) into piwigo/galleries'] = 'Skopiuj zawartość g2data/albums (Gallery2) lub var/albums (Gallery3) do piwigo/galleries';
+$lang['Menalto2Piwigo plugin imports data from a Menalto Gallery2/Gallery3 installation into Piwigo.'] = 'Plugin Menalto2Piwigo importuje dane z Menalto Gallery2/Gallery3 do Piwigo';
+$lang['Submit the form at the end of this page'] = 'Wyślij formularz na końcu tej strony';
+$lang['Title and description for photos'] = 'Tytuł i opis zdjęć';
+$lang['User comments on photos'] = 'Komentarze użytkownika pod zdjęciami';
+$lang['useless on Gallery3'] = 'bezużytecnzy na Gallery3';
+$lang['How to use it?'] = 'Jak używać plugina?';
+$lang['Import'] = 'Importuj';
+$lang['Import from Menalto'] = 'Importuj z Menalto';
+$lang['Imported properties:'] = 'Zaimportowane właściwości:';
+$lang['Install and activate plugins:'] = 'Zainstaluj i aktywuj pluginy:';
+$lang['Name and description for albums'] = 'Nazwa i opis albumów';
+$lang['No Menalto tables found!'] = 'Nie odnaleziono tabel Menalto';
+$lang['Start import'] = 'Rozpocznij import';
+$lang['column prefix'] = 'prefiks kolumny';
+$lang['database host'] = 'host bazy danych';
+$lang['database name'] = 'nazwa bazy';
+$lang['database password'] = 'hasło bazy';
+$lang['database user'] = 'użytkownik bazy';
+$lang['table prefix'] = 'prefiks tabel';
