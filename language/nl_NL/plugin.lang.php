ff6f86532dbe23422a4fa0aa1b460b0009895666
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-08-21 06:46:54 +0200

    Update nl_NL, thanks to watertoren5044

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -20,17 +20,17 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['Copy the content of g2data/albums (Gallery2) or var/albums (Gallery3) into piwigo/galleries'] = 'Kopieer de inhoud van de g2data/albums (Gallery2) of var/albums (Gallery3) naar piwigo/galerieën';
+$lang['Copy the content of g2data/albums (Gallery2) or var/albums (Gallery3) into piwigo/galleries'] = 'Kopieer de inhoud van de g2data/albums (Galerie2) of var/albums (Galerie3) naar piwigo/galerieën';
 $lang['How to use it?'] = 'Hoe te gebruiken?';
 $lang['Import'] = 'Importeren';
 $lang['Import from Menalto'] = 'Importeer van Menalto';
 $lang['Imported properties:'] = 'Geïmporteerde eigenschappen:';
 $lang['Install and activate plugins:'] = 'Installeer en activeer plugins:';
-$lang['Menalto2Piwigo plugin imports data from a Menalto Gallery2/Gallery3 installation into Piwigo.'] = 'Menalto2Piwigo plugins importeren data van een Menalto Gallery2/Gallery3 installatie naar Piwigo.';
+$lang['Menalto2Piwigo plugin imports data from a Menalto Gallery2/Gallery3 installation into Piwigo.'] = 'Menalto2Piwigo plugins importeren data van een Menalto Galerie2/Galerie3 installatie naar Piwigo.';
 $lang['Name and description for albums'] = 'Naam en beschrijvingen van albums';
 $lang['No Menalto tables found!'] = 'Geen Menalto-tabellen gevonden!';
 $lang['Start import'] = 'Start het importeren';
-$lang['Submit the form at the end of this page'] = 'Verstuur het formulier op het einde van deze pagina';
+$lang['Submit the form at the end of this page'] = 'Verstuur het formulier bij het einde van deze pagina';
 $lang['Title and description for photos'] = 'Titel en beschrijving van foto\'s';
 $lang['User comments on photos'] = 'Reacties van gebruikers op foto\'s';
 $lang['column prefix'] = 'kolom-voorvoegsel';
@@ -39,4 +39,4 @@ $lang['database name'] = 'databasenaam';
 $lang['database password'] = 'database-wachtwoord';
 $lang['database user'] = 'databasegebruiker';
 $lang['table prefix'] = 'tabel-voorvoegsel';
-$lang['useless on Gallery3'] = 'nutteloos voor Gallery3';
+$lang['useless on Gallery3'] = 'nutteloos voor Galerie3';
