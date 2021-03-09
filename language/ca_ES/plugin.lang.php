4f78cbbf258ed3b6ae48ad9b88b409f2e7ee79ab
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-03-09 15:35:49 +0100

    Update ca_ES, thanks to Sebastia

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Copy the content of g2data/albums (Gallery2) or var/albums (Gallery3) into piwigo/galleries'] = 'Copieu el contingut de g2data/albums (Gallery2) o var/albums (Gallery3) a piwigo/galleries';
+$lang['How to use it?'] = 'Com utilitzar-ho?';
+$lang['Import'] = 'Importa';
+$lang['Import from Menalto'] = 'Importa des de Menalto';
+$lang['Imported properties:'] = 'Propietats importades:';
+$lang['Install and activate plugins:'] = 'Instal·la i activa els connectors:';
+$lang['Menalto2Piwigo plugin imports data from a Menalto Gallery2/Gallery3 installation into Piwigo.'] = 'Els connectors Menalto2Piwigo importen dades d\'una instal·lació Menalto Gallery2/Gallery3 a Piwigo.';
+$lang['Name and description for albums'] = 'Nom i descripció dels àlbums';
+$lang['No Menalto tables found!'] = 'No s\'ha trobat cap taula Menalto!';
+$lang['Start import'] = 'Inicia la importació';
+$lang['Submit the form at the end of this page'] = 'Envia el formulari al final d\'aquesta pàgina';
+$lang['Title and description for photos'] = 'Títol i descripció de les fotografies';
+$lang['User comments on photos'] = 'Comentaris de l\'usuari a les fotos';
+$lang['column prefix'] = 'prefix de columna';
+$lang['database host'] = 'amfitrió de la base de dades';
+$lang['database name'] = 'nom de la base de dades';
+$lang['database password'] = 'contrasenya de la base de dades';
+$lang['database user'] = 'usuari de la base de dades';
+$lang['table prefix'] = 'prefix de taula';
+$lang['useless on Gallery3'] = 'inútil a Gallery3';
