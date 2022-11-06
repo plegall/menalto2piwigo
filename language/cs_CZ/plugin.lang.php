8d344ec285c4e688f9b8c8d123bd587a81748d6f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-06 16:16:01 +0100

    Add cs_CZ, thanks to gandycz

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Copy the content of g2data/albums (Gallery2) or var/albums (Gallery3) into piwigo/galleries'] = 'Zkopírujte obsah g2data/alb (Gallery2) nebo var/album (Gallery3) do piwigo/galleries';
+$lang['How to use it?'] = 'Jak to použít?';
+$lang['Import'] = 'Importovat';
+$lang['Import from Menalto'] = 'Importovat z Menalto';
+$lang['Imported properties:'] = 'Importované nastavení:';
+$lang['Install and activate plugins:'] = 'Instalovat a activovat pluginy:';
+$lang['Menalto2Piwigo plugin imports data from a Menalto Gallery2/Gallery3 installation into Piwigo.'] = 'Zásuvné moduly Menalto2Piwigo importují data z instalace Menalto Gallery2/Gallery3 do Piwigo.';
+$lang['Name and description for albums'] = 'Název a popis alb';
+$lang['No Menalto tables found!'] = 'Nebyly nalezeny žádné tabulky Menalto!';
+$lang['Start import'] = 'Spustit import';
+$lang['Submit the form at the end of this page'] = 'Odeslat formulář na konci této stránky';
+$lang['Title and description for photos'] = 'Název a popis pro fotografie';
+$lang['User comments on photos'] = 'Uživatelské komentáře u fotografií';
+$lang['column prefix'] = 'prefix sloupce';
+$lang['database host'] = 'hostitel databáze';
+$lang['database name'] = 'jméno databáze';
+$lang['database password'] = 'heslo databáze';
+$lang['database user'] = 'uživatel databáze';
+$lang['table prefix'] = 'prefix tabulek';
+$lang['useless on Gallery3'] = 'k ničemu v Gallery3';
