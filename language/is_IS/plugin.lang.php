21bfc40b88a7a0200a960928da63ce1a662a2dd8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-04-26 06:24:56 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Copy the content of g2data/albums (Gallery2) or var/albums (Gallery3) into piwigo/galleries'] = 'Afritaðu innihald g2data/albums (Gallery2) eða var/albums (Gallery3) í piwigo/galleries';
+$lang['How to use it?'] = 'Hvernig á að nota það?';
+$lang['Import'] = 'Flytja inn';
+$lang['Import from Menalto'] = 'Innflutningur frá Menalto';
+$lang['Imported properties:'] = 'Innfluttir eiginleikar:';
+$lang['Install and activate plugins:'] = 'Settu upp og virkjaðu viðbætur:';
+$lang['Menalto2Piwigo plugin imports data from a Menalto Gallery2/Gallery3 installation into Piwigo.'] = 'Menalto2Piwigo viðbótin flytur inn gögn frá Menalto Gallery2/Gallery3 lausnum inn í Piwigo.';
+$lang['Name and description for albums'] = 'Heiti og lýsing á albúmum';
+$lang['No Menalto tables found!'] = 'Engar Menalto töflur fundust!';
+$lang['Start import'] = 'Byrjaðu innflutning';
+$lang['Submit the form at the end of this page'] = 'Sendu eyðublaðið í lok þessarar vefsíðu';
+$lang['Title and description for photos'] = 'Titill og lýsing á myndum';
+$lang['User comments on photos'] = 'Notandaumsagnir við myndir';
+$lang['column prefix'] = 'dálkaforskeyti';
+$lang['database host'] = 'gagnagrunnur hýsingaraðila';
+$lang['database name'] = 'gagnagrunnsheiti';
+$lang['database password'] = 'gagnagrunnslykilorð';
+$lang['database user'] = 'gagnagrunnsnotandi';
+$lang['table prefix'] = 'töfluforskeyti';
+$lang['useless on Gallery3'] = 'gagnslaust á Gallery3';
