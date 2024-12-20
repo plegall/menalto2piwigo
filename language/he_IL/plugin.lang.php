e6ef4c6ffde3ee29fc0c9e9e8d7c40aa6b1b5818
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 16:05:04 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Copy the content of g2data/albums (Gallery2) or var/albums (Gallery3) into piwigo/galleries'] = 'העתק את תוכן g2data/albums (Gallery2) או var/albums (Gallery3) לתיקיית piwigo/galleries';
+$lang['How to use it?'] = 'כיצד להשתמש בזה?';
+$lang['Import'] = 'יבוא';
+$lang['Import from Menalto'] = 'יבוא מ-Menalto';
+$lang['Imported properties:'] = 'מאפיינים שיובאו:';
+$lang['Install and activate plugins:'] = 'התקן והפעל תוספים:';
+$lang['Menalto2Piwigo plugin imports data from a Menalto Gallery2/Gallery3 installation into Piwigo.'] = 'תוספי Menalto2Piwigo מייבאים נתונים מהתקנת Gallery2/Gallery3 של Menalto ל-Piwigo.';
+$lang['Name and description for albums'] = 'שם ותיאור לאלבומים';
+$lang['No Menalto tables found!'] = 'לא נמצאו טבלאות של Menalto!';
+$lang['Start import'] = 'התחל את היבוא';
+$lang['Submit the form at the end of this page'] = 'שלח את הטופס בסופו של עמוד זה';
+$lang['Title and description for photos'] = 'כותרת ותיאור לתמונות';
+$lang['User comments on photos'] = 'תגובות משתמשים על תמונות';
+$lang['column prefix'] = 'קידומת עמודה';
+$lang['database host'] = 'מארח בסיס הנתונים';
+$lang['database name'] = 'שם בסיס הנתונים';
+$lang['database password'] = 'סיסמת בסיס הנתונים';
+$lang['database user'] = 'משתמש בסיס הנתונים';
+$lang['table prefix'] = 'קידומת טבלה';
+$lang['useless on Gallery3'] = 'לא רלוונטי ב-Gallery3';
