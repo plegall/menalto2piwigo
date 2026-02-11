0b9ae2f705c47cb184e4cfeba71db30840cb4087
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-11 06:58:47 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Copy the content of g2data/albums (Gallery2) or var/albums (Gallery3) into piwigo/galleries'] = 'g2data/albums (Gallery2) 또는 var/albums (Gallery3)의 콘텐츠를 piwigo/galleries로 복사합니다.';
+$lang['How to use it?'] = '어떻게 사용하나요?';
+$lang['Import'] = '가져오기';
+$lang['Import from Menalto'] = 'Menalto에서 가져오기';
+$lang['Imported properties:'] = '가져온 속성:';
+$lang['Install and activate plugins:'] = '플러그인을 설치하고 활성화합니다:';
+$lang['Menalto2Piwigo plugin imports data from a Menalto Gallery2/Gallery3 installation into Piwigo.'] = 'Menalto2Piwigo 플러그인은 Menalto Gallery2/Gallery3 설치에서 Piwigo로 데이터를 가져옵니다.';
+$lang['Name and description for albums'] = '앨범 이름 및 설명';
+$lang['No Menalto tables found!'] = 'Menalto 테이블을 찾을 수 없습니다!';
+$lang['Start import'] = '가져오기 시작';
+$lang['Submit the form at the end of this page'] = '이 페이지 끝에 있는 양식을 제출하세요.';
+$lang['Title and description for photos'] = '사진의 제목 및 설명';
+$lang['User comments on photos'] = '사진에 대한 사용자 댓글';
+$lang['column prefix'] = '컬럼 접두사';
+$lang['database host'] = '데이터베이스 호스트';
+$lang['database name'] = '데이터베이스 이름';
+$lang['database password'] = '데이터베이스 비밀번호';
+$lang['database user'] = '데이터베이스 사용자';
+$lang['table prefix'] = '테이블 접두사';
+$lang['useless on Gallery3'] = 'Gallery3에서는 쓸모없음';
